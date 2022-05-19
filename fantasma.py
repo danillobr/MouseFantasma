04c74ce089bdb60142aa578b0b2942a066ea4a58
“Danillo” <danillo.abreu@arapiraca.ufal.br>
2022-05-19 09:23:57 -0300

    fantsama pronto

--- fantasma.py
+++ fantasma.py
@@ -1,15 +1,34 @@
-import re
-import pyautogui
 from time import sleep
-    
+from pynput.mouse import Controller
+
+mouse = Controller()
+ultimaPosicaoMouse = mouse.position
+direcao = True
+
+def verificarPosicaoMouse():
+    global mouse
+    return mouse.position
+
+def verificarMouseParadoDoisMinutos():
+    global ultimaPosicaoMouse
+
+    sleep(3)
+    if(ultimaPosicaoMouse == verificarPosicaoMouse()):
+        ultimaPosicaoMouse = verificarPosicaoMouse()
+        ativarFantasma()        
+    else:
+        ultimaPosicaoMouse = verificarPosicaoMouse()
+    return verificarMouseParadoDoisMinutos()
+
 def ativarFantasma():
-    pyautogui.move(500,-100)
-    pyautogui.move(-500,100)
-    sleep(0.5)
+    global direcao
+
+    if(direcao):
+        mouse.move(300,20)
+        direcao = False
+    else:
+        mouse.move(-300,-20)
+        direcao = True
     return
 
-ativarFantasma()
-ativarFantasma()
-ativarFantasma()
-ativarFantasma()
-ativarFantasma()
+verificarMouseParadoDoisMinutos()
