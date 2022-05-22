8e1bc5b5d18860632b49a0ef754e969315f51ad9
Danillo Rodrigues <danilo100kl@hotmail.com>
2022-05-22 13:41:13 -0300

    adicionando a funcionalidade de atualizar a página.

--- fantasma.py
+++ fantasma.py
@@ -1,15 +1,39 @@
-import re
-import pyautogui
 from time import sleep
-    
+import pynput.mouse as ms
+import pynput.keyboard as tc
+
+mouse = ms.Controller()
+teclado = tc.Controller()
+ultimaPosicaoMouse = mouse.position
+direcao = True
+
+def verificarPosicaoMouse():
+    global mouse
+    return mouse.position
+
+def verificarMouseParadoDoisMinutos():
+    global ultimaPosicaoMouse
+
+    sleep(55)
+    if(ultimaPosicaoMouse == verificarPosicaoMouse()):
+        ultimaPosicaoMouse = verificarPosicaoMouse()
+        ativarFantasma()        
+    else:
+        ultimaPosicaoMouse = verificarPosicaoMouse()
+    return verificarMouseParadoDoisMinutos()
+
 def ativarFantasma():
-    pyautogui.move(500,-100)
-    pyautogui.move(-500,100)
-    sleep(0.5)
+    global direcao
+    global teclado
+
+    if(direcao):
+        mouse.move(300,20)
+        direcao = False
+    else:
+        mouse.move(-300,-20)
+        direcao = True
+    teclado.press(tc.Key.f5)
+    teclado.release(tc.Key.f5)
     return
 
-ativarFantasma()
-ativarFantasma()
-ativarFantasma()
-ativarFantasma()
-ativarFantasma()
+verificarMouseParadoDoisMinutos()
